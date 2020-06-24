69a65a3e0fc5339c87a2e8615ea33344d2516586
vijaykilledar <vijay.killedar@gmail.com>
2020-06-24 22:27:30 +0530

    NA:Adding placeholder main() which creates daemon process.

--- src/net_interface.h
+++ src/net_interface.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-enum class Net_Interface_Status {
+enum class NetIntfStatus {
     down    = 0,
     up      = 1,
     invalid = 2
@@ -13,7 +13,13 @@ class NetInterface {
     private:
         std::string m_ipv4_addr;
         std::string m_ipv6_addr;
+        NetIntfStatus m_link_status;
+        NetIntfStatus m_link_admin_status;
     public:
+        const std::string& get_ipv4_addr();
+        const std::string& get_ipv6_addr();
+        const NetIntfStatus& get_link_status();
+        const m_link_admin_status& get_link_admin_status();
 };
 
 #endif // NET_INTERFACE_H
